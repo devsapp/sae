9d23835b4337044756486bcf1632df601ff89bc5
xsahxl <xsahxl@126.com>
2022-08-08 22:35:03 +0800

    #25 使用esbuild 打包成一个bundle进行 组件加速下载

--- lib/lib/help/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './deploy';
-export * from './info';
-export * from './remove';
--- lib/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function uploadFile(credentials: any, codePackage: any, object: any, type: any): Promise<void>;
-export declare function handleCode(Region: any, Application: any, credentials: any): Promise<{
-    applicationObject: any;
-    codePackage: any;
-    tempObject: string;
-}>;
