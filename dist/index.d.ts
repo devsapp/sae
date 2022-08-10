e9b650b9d68d102fb44f53158534cc5a1c79df77
wss-git <wssgryx@163.com>
2022-08-10 14:21:32 +0800

    build: build dev

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { InputProps } from './common/entity';
-export default class SaeComponent {
-    start(inputs: InputProps): Promise<import("./common/entity").OutputProps>;
-    stop(inputs: InputProps): Promise<void>;
-    info(inputs: InputProps): Promise<import("./common/entity").OutputProps>;
-    deploy(inputs: InputProps): Promise<import("./common/entity").OutputProps>;
-    remove(inputs: InputProps): Promise<void>;
-}
