980a127f5a8bda8544cd152b15668bc3d9950c24
pichunying <2339348937@qq.com>
2022-08-10 15:33:57 +0800

    fix: securityGroupId为空

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { InputProps } from './interface/entity';
-export default class SaeComponent {
-    start(inputs: InputProps): unknown;
-    stop(inputs: InputProps): any;
-    info(inputs: InputProps): unknown;
-    deploy(inputs: InputProps): unknown;
-    remove(inputs: InputProps): any;
-}
